c82a92d90a7b60eb0b074bf98aea7bba0e0fd3b9
Oziel <Oziel@DESKTOP-N1P5O6F>
2018-01-31 22:33:35 -0200

    Classes de Paginação

---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+
+        public int ItensPorPagina { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+        public int TotalPagina
+        {
+            get
+            {
+                // Ceiling => arredonda para cima
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            }
+        }
+    }
+}
